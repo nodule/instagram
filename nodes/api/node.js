5d4755c5084b8f0a6d07c0a9cb9092d427becfd3
Rob Halff <rob.halff@gmail.com>
2014-02-20 16:15:54 +0100

    make client_id non required

--- nodes/api/node.js
+++ nodes/api/node.js
@@ -1,3 +1,3 @@
-instagram_node_lib.set('client_id', input.client_id);
+if(input.client_id) instagram_node_lib.set('client_id', input.client_id);
 if(input.client_secret) instagram_node_lib.set('client_secret', input.client_secret);
 output.instagram = instagram_node_lib;
